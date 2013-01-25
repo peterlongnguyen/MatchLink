4ad33bb24779cb6b088e660da89c68fd464b1101
peterlnguyen <lpnguyen@usc.edu>
2013-01-24 22:44:00 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KisnetRails::Application.config.secret_token = '873a1ad2f6c2a586eb1dd71ff2f9d63ebd26d75077f4eef87afddef59380998a8699af32ffb68ad0283fcb77b09b00c710bfa4a8995d9e2b37c217a98da48e75'
